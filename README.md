64f913080e733ed524a7d8e4436248edb1d9bbc4
ATofighi <alirtofighim@gmail.com>
2013-12-31 15:54:07 +0330

    رفع مشکل EOL فایل ها

--- README.md
+++
@@ -1,7 +0,0 @@
-MyBBPersian
-===========
-
-MyBB Persian Translate
-
-Translate By: SaeedGH And MY-BB.IR Group...
-Support in: http://my-bb.ir
--- inc/languages/persian.php
+++ inc/languages/persian.php
@@ -4,7 +4,7 @@
 $langinfo['name'] = "فارسی (Persian)";
 
 /* The author of the language */
-$langinfo['author'] = "My-BB.Ir Group & IORA.Ir";
+$langinfo['author'] = "My-BB.Ir Group & iora.ir";
 
 /* The language authors website */
 $langinfo['website'] = "http://mybb.com/";
