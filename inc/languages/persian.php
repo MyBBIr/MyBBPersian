e79d48175c65d178e799a86dd27f72d3cd8df121
ATofighi <alirtofighim@gmail.com>
2014-08-02 01:08:39 +0430

    آماده سازی برای ۱.۶.۱۵

--- inc/languages/persian.php
+++ inc/languages/persian.php
@@ -4,13 +4,13 @@
 $langinfo['name'] = "فارسی (Persian)";
 
 /* The author of the language */
-$langinfo['author'] = "My-BB.Ir Group & iora.ir";
+$langinfo['author'] = "My-BB.Ir Group & IORA.Ir";
 
 /* The language authors website */
 $langinfo['website'] = "http://mybb.com/";
 
 /* Compatible version of MyBB */
-$langinfo['version'] = "1614";
+$langinfo['version'] = "1615";
 
 /* Sets if the translation includes the Admin CP (1 = yes, 0 = no) */
 $langinfo['admin'] = 1;
