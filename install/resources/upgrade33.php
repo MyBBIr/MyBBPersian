d564323baebb41d03e5ffe215ba9d2ab1e1d9f18
ATofighi <alirtofighim@gmail.com>
2015-05-28 02:39:37 +0430

    MyBB 1805

---
+++ install/resources/upgrade33.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * MyBB 1.8
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ * Website: http://www.mybb.com
+ * License: http://www.mybb.com/about/license
+ *
+ */
+
+/**
+ * Upgrade Script: 1.8.4
+ */
+
+$upgrade_detail = array(
+	"revert_all_templates" => 0,
+	"revert_all_themes" => 0,
+	"revert_all_settings" => 0
+);
+
+@set_time_limit(0);
+
+function upgrade33_dbchanges()
+{
+	global $db, $output;
+
+	$output->print_header("بروزرسانی پایگاه‌داده");
+	echo "<p>انجام کارهای ارتقای لازم...</p>";
+	flush();
+
+	if($db->field_exists('2fasecret', 'adminoptions'))
+	{
+		$db->rename_column('adminoptions', '2fasecret', 'authsecret', "varchar(16) NOT NULL default ''");
+	}
+	
+	$output->print_contents("<p>برای ادامه‌ی فرایند بروزرسانی بر روی بعدی کلیک کنید.</p>");
+	$output->print_footer("33_done");
+}
